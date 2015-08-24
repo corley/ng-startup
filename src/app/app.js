305170dc60d196ac4ab912c1205271f325500890
Gianluca Arbezzano <gianarb92@gmail.com>
2015-08-24 15:42:03 +0200

    Manage integration of cr-identity

--- src/app/app.js
+++ src/app/app.js
@@ -24,6 +24,7 @@ angular.module(
             'cr.loading',
             'cr.session',
             'cr.acl',
+            'cr.identity',
             'satellizer',
             'pascalprecht.translate',
             'infinite-scroll',
@@ -102,91 +103,4 @@ function run($rootScope, crAcl, crSession, crRemoteHttp, crIdentity, $state, $lo
     $scope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
         cfpLoadingBar.start();
     });
-}])
-
-
-.service('crIdentity', ['crSession', 'crAcl', '$rootScope', '$log', '$q', function(crSession, crAcl, $rootScope, $log, $q) {
-    var self = this;
-
-    self.getSign = function(type) {
-      var auth = crSession.get('identity', "cr-identity");
-      if(auth && auth.type && auth.token) {
-        return auth;
-      }
-      else {
-        return false;
-      }
-    };
-
-    self.set = function(identity)
-    {
-        $log.debug("[crIdentity] Identity:", identity);
-        crSession.set('identity', identity, "cr-identity");
-        $rootScope._identity = identity;
-    };
-
-    /**
-    * Return identity from session
-    * @return Object
-    */
-    self.get = function()
-    {
-      return crSession.get('identity', "cr-identity");
-    };
-
-    /**
-    * Clear auth session
-    */
-    self.purge = function()
-    {
-      delete $rootScope._identity;
-      return crSession.purgeNamespace("cr-identity");
-    };
-
-
-    self.restore = function() {
-      var d = $q.defer();
-      var identity = self.get();
-      if(!identity) {
-        d.reject();
-      }
-      else if(identity && identity.role) {
-        crAcl.setRole(identity.role);
-      }
-      $rootScope._identity = identity;
-      //crSession.delete('satellizer_token');
-
-      $rootScope.$emit("auth:restore:success", identity);
-      $log.debug("[crIdentity] Broadcast auth:restore:success");
-
-      d.resolve();
-      return d.promise;
-    };
-
-
-    $rootScope.$on('auth:login:success', function(event, data) {
-      self.set(data);
-      // crAcl.setCredentials(data.auth);
-      if(data.role) {
-        crAcl.setRole(data.role);
-      }
-      $rootScope.$broadcast('auth:identity:success', data);
-      $log.debug("[crAuth] Broadcast auth:identity:success");
-    });
-
-    $rootScope.$on('auth:logout:success', function(event, data) {
-      self.purge();
-      // crAcl.voidCredentials();
-      crAcl.setRole();
-      $rootScope.$broadcast('auth:purge:success', data);
-      $log.debug("[crAuth] Broadcast auth:purge:success");
-
-    });
-
-
-
-    return self;
-}])
-
-
-;
+}]);
