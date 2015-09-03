aeb42c99885aafa853c1b503e776dcc581dac57e
Gabriele Mittica <mittica.gabriele@gmail.com>
2015-09-03 10:02:25 +0200

    layout common templates

--- src/common/layout/layout.js
+++ src/common/layout/layout.js
@@ -4,9 +4,43 @@ angular.module('ng-startup.layout', [])
     restrict: "AE",
     replace: true,
     scope: {
-      username: "="
+      identity: "=",
+      state: "="
     },
     templateUrl: "layout/header.tpl.html"
+  };
+}])
+.directive('left', [function() {
+  return {
+    restrict: "AE",
+    replace: true,
+    scope: {
+      identity: "=",
+      state: "="
+    },
+    templateUrl: "layout/left.tpl.html"
+  };
+}])
+.directive('right', [function() {
+  return {
+    restrict: "AE",
+    replace: true,
+    scope: {
+      identity: "=",
+      state: "="
+    },
+    templateUrl: "layout/right.tpl.html"
+  };
+}])
+.directive('footer', [function() {
+  return {
+    restrict: "AE",
+    replace: true,
+    scope: {
+      identity: "=",
+      state: "="
+    },
+    templateUrl: "layout/footer.tpl.html"
   };
 }])
 ;
