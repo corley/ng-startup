9b08cff6c9684fdbfbf6bbdbc236bb3b7461ce07
Gabriele Mittica <mittica.gabriele@gmail.com>
2015-10-19 09:50:29 +0200

    fix

--- src/app/app.js
+++ src/app/app.js
@@ -10,7 +10,6 @@ angular.module(
             'LocalStorageModule',
             'ngAnimate',
             'ngSanitize',
-            'highcharts-ng',
             'angular-loading-bar',
             'templates-app',
             'templates-common',
@@ -18,15 +17,12 @@ angular.module(
             'ui.router',
             'ng-startup.appConf',
             'ng-startup.home',
-            'ng-startup.dashboard',
-            'ng-startup.signin',
             'ng-startup.layout',
             'cr.remote',
             'cr.loading',
             'cr.session',
             'cr.acl',
             'cr.identity',
-            'ng-startup.paginator',
             'satellizer',
             'pascalprecht.translate',
             'infinite-scroll',
@@ -50,9 +46,6 @@ angular.module(
     //set api andpoint by app config
     crRemoteProvider.addEndpoint("default", appConf.endPoint);
 
-    $authProvider.loginUrl = appConf.endPoint + 'login';
-    $authProvider.authToken = 'Basic';
-
     //enable debug state
     $logProvider.debugEnabled(appConf.debug);
 
@@ -67,42 +60,15 @@ angular.module(
 .run(['$rootScope', 'crAcl', 'crSession', 'crRemoteHttp', 'crIdentity', '$state', '$log',
 function run($rootScope, crAcl, crSession, crRemoteHttp, crIdentity, $state, $log) {
 
-    //set default login state for unauth users
-    crAcl.setRedirect("signin");
 
-
-
-    //what append on user successful login
-    $rootScope.$on('auth:identity:success', function(event, data) {
-      $state.go("dashboard");
-    });
-
-    //what append when user refresh the page and identity is restored
-
-    crIdentity.restore().then(function(identity) {
-
-    });
-
-    //what append on user logout
-    $rootScope.$on("auth:purge:success", function(event, data){
-      $state.go("home");
-    });
-
-
-    document.addEventListener("deviceready", function(){
-      // This event is essential to any application. It signals that Cordova's device APIs have loaded and are ready to access.
-    });
 }])
 
 .controller('AppCtrl', ['$scope','$rootScope', '$location', '$state', 'cfpLoadingBar',
   function AppCtrl($scope, $rootScope, $location, $state, cfpLoadingBar) {
 
-
     $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
         $rootScope._state = toState;
         cfpLoadingBar.complete();
-        //switch on/off black design
-
     });
     $scope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
         cfpLoadingBar.start();
